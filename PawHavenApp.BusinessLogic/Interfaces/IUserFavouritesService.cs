6445df71efe16cabc5d22ceb11947d722fa88377
klasterr <kuzjovictor@gmail.com>
2025-04-12 17:55:39 +0300

    Setup pet favourites

---
+++ PawHavenApp.BusinessLogic/Interfaces/IUserFavouritesService.cs
@@ -0,0 +1,15 @@
+﻿namespace PawHavenApp.BusinessLogic.Interfaces;
+
+using PawHavenApp.BusinessLogic.Models;
+using PawHavenApp.DataAccess.Entities;
+
+public interface IUserFavouritesService
+{
+    Task<int?> CreateAsync(int petCardId, Guid userId);
+
+    Task DeleteAsync(int petCardId, Guid userId);
+
+    Task<List<PetCardModel>> GetAllFavouritesByUser(Guid userId);
+
+    Task<bool> IsFavouriteExists(int petCardId, Guid Id);
+}
